c17bdbc21a247e7b8353e4dcf1b85736fa4da057
vagrant <vagrant@shirebrookline-com>
2020-03-09 00:45:08 +0000

    Updated scss.:

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    @include('partials.head')
+    <body>
+    @include('partials.nav')
+    @include('partials.main')
+    @include('partials.footer')
+    </body>
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container-fluid text-center">
+        <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+</footer>
+<script src="{{ asset('js/app.js') }}"></script>
