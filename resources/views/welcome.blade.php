e2eb2b790c1c7f5a815a3c54edb5097bc26e7b48
Gary <gary.rancier@newellco.com>
2020-03-11 16:45:50 -0400

    Updates to logo font css.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.default')
 @section('content')
-    i am the home page
+    <div class="row mt-5 logo-hero">
+        <div class="main">
+            <span class="logo">Shirebrook</span>
+            <span class="cursive">/Line</span>
+        </div>
+        <p class="slug">Digital Experience Consultants</p>
+    </div>
 @stop
